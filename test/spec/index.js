088ade5ee7e03b0bf94723c48ab5ba07d2b2abda
Anton <anton@adc.sh>
2018-05-03 07:36:09 +0100

    add setup middleware test

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,5 +1,7 @@
+const rqt = require('rqt')
 const { equal } = require('zoroaster/assert')
 const context = require('../context')
+const { ContextAPI } = require('../context/types') // eslint-disable-line
 
 const idioTestSuite = {
   context,
@@ -13,6 +15,33 @@ const idioTestSuite = {
   async 'connects to a database'({ start }) {
     const { connect } = await start()
     equal(connect, undefined)
+  },
+  /**
+   * @param {ContextAPI} api
+   */
+  async 'sets up middleware correctly'(api) {
+    const { start } = api
+    let called
+    const f = Math.random() * 1000
+    const m = 100
+    const { url } = await start({
+      host: '0.0.0.0',
+      middleware: {
+        simpleRoute: {
+          async function(app, { multiplier }) {
+            return (ctx) => {
+              called = true
+              ctx.body = f * multiplier
+            }
+          },
+          config: { multiplier: m },
+          use: true,
+        },
+      },
+    })
+    const res = await rqt(url)
+    equal(called, true)
+    equal(res, f * m)
   },
 }
 
