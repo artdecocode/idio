96effddbdae0b7a1b723a5e9f5fd56816257630b
Anton <anton@adc.sh>
2018-05-18 21:44:40 +0100

    update info script

--- info.js
+++ info.js
@@ -1,5 +1,9 @@
 const { spawnSync } = require('child_process')
 
+const param = process.argv[3] || 'homepage'
+
 const { stdout } = spawnSync('yarn', ['info', process.argv[2], '--json'])
-const { data: { homepage } } = JSON.parse(stdout)
-console.log(homepage)
+const { data } = JSON.parse(stdout)
+
+const d = data[param]
+console.log(d)
